92e0791803159b7de37d800e82308284a893911f
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2015-06-08 18:29:55 -0300

    Docstrings in accordance to pep 257

--- catsim/cat/irt.py
+++ catsim/cat/irt.py
@@ -2,8 +2,7 @@ import math
 
 
 def tpm(theta, a, b, c):
-    """
-    Item Response Theory three-parameter logistic function:
+    """Item Response Theory three-parameter logistic function:
 
     .. math:: P(X_i = 1| \\theta) = c_i + \\frac{1-c_i}{1+ e^{Da_i(\\theta-b_i)}}
 
@@ -29,13 +28,14 @@ def tpm(theta, a, b, c):
 
 
 def logLik(est_theta, response_vector, administered_items):
-    """
-    Calculates the log-likelihood of an estimated proficiency, given a
+    """Calculates the log-likelihood of an estimated proficiency, given a
     response vector and the parameters of the answered items.
 
     .. math:: L(X_{Ij} | \\theta_j, a_I, b_I, c_I) = \\prod_{i=1} ^ I P_{ij}(\\theta)^{X_{ij}} Q_{ij}(\\theta)^{1-X_{ij}}
 
-    For computational reasons, it is common to use the log-likelihood in maximization/minimization problems, transforming the product of probabilities in a sum of probabilities:
+    For computational reasons, it is common to use the log-likelihood in
+    maximization/minimization problems, transforming the product of
+    probabilities in a sum of probabilities:
 
     .. math:: \\log L(X_{Ij} | \\theta_j, , a_I, b_I, c_I) = \\sum_{i=1} ^ I \\left\\lbrace x_{ij} \\log P_{ij}(\\theta)+ (1 - x_{ij}) \\log Q_{ij}(\\theta) \\right\\rbrace
     """
@@ -54,16 +54,14 @@ def logLik(est_theta, response_vector, administered_items):
 
 
 def negativelogLik(est_theta, args):
-    """
-    Function used by `scipy.optimize.minimize` to find the estimated
+    """Function used by `scipy.optimize.minimize` to find the estimated
     proficiency that maximizes the likelihood of a given response vector
     """
     return -logLik(est_theta[0], args[0], args[1])
 
 
 def inf(theta, a, b, c):
-    """
-    Item Response Theory three-parameter information function
+    """Item Response Theory three-parameter information function
 
     .. math:: I(\\theta) = a^2\\frac{(P(\\theta)-c)^2}{(1-c)^2}.\\frac{(1-P(\\theta))}{P(\\theta)}
 
