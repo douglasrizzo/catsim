ea937544f7b215f9d806d1ac78b890516da4dc09
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2015-06-15 01:04:19 -0300

    Added documentation compatible with Sphinx

--- catsim/cluster/stats.py
+++ catsim/cluster/stats.py
@@ -20,8 +20,7 @@ def means(x, clusters):
 
 
 def variances(x, clusters):
-    """
-    Calculates the variance for each cluster
+    """Calculates the variance for each cluster
 
     .. math:: \\sigma^2_g = \\frac{1}{N_g}\\sum_{i=1; j=i+1}^{N_g} (\mu_g - d(i, j))
     """
@@ -40,8 +39,7 @@ def variances(x, clusters):
 
 
 def mean_variance(x, clusters):
-    """
-    Returns the mean variance for all clusters
+    """Returns the mean variance for all clusters
 
     .. math:: \\sigma^2 = \\frac{1}{G}\\sum_{g=1}^G \\frac{1}{N} \\sum_{i=1; j=i+1}^N (\mu_g - d(i, j))
     """
@@ -49,26 +47,20 @@ def mean_variance(x, clusters):
 
 
 def dunn(c, distances):
-    """
-    Dunn index for cluster validation (the bigger, the better)
+    """Dunn index for cluster validation (the bigger, the better)
 
-    .. math:: D = \\min_{i = 1 \\ldots n_c; j = i + 1\ldots n_c}
-    \\left\\lbrace
-    \\frac{d \\left( c_i,c_j \\right)}{\\max_{k = 1 \\ldots n_c} \\left(diam
-    \\left(c_k \\right) \\right)} \\right\\rbrace
+    .. math:: D = \\min_{i = 1 \\ldots n_c; j = i + 1\ldots n_c}\\left\\lbrace\\frac{d \\left( c_i,c_j \\right)}{\\max_{k = 1 \\ldots n_c} \\left(diam \\left(c_k \\right) \\right)} \\right\\rbrace
 
     where :math:`d(c_i,c_j)` represents the distance between clusters
     :math:`c_i` and :math:`c_j`, given by the distances between its two
-        closest data points, and :math:`diam(c_k)` is the diameter of cluster
+    closest data points, and :math:`diam(c_k)` is the diameter of cluster
     :math:`c_k`, given by the distance between its two farthest data points.
 
     The bigger the value of the resulting Dunn index, the better the
     clustering result is considered, since higher values indicate that
     clusters are compact (small :math:`diam(c_k)`) and far apart.
 
-    .. [Kovacs2005] Kovács, F., Legány, C., & Babos, A. (2005). Cluster
-    validity measurement techniques. 6th International Symposium of Hungarian
-    Researchers on Computational Intelligence.
+    .. [Kov05] Kovács, F., Legány, C., & Babos, A. (2005). Cluster validity measurement techniques. 6th International Symposium of Hungarian Researchers on Computational Intelligence.
     """
     unique_cluster_distances = np.unique(min_cluster_distances(c, distances))
     max_diameter = max(diameter(c, distances))
@@ -80,8 +72,7 @@ def dunn(c, distances):
 
 
 def min_cluster_distances(c, D):
-    """
-    Calculates the distances between the two nearest points of each cluster.
+    """Calculates the distances between the two nearest points of each cluster.
     """
 
     # creates empty matrix
@@ -107,8 +98,7 @@ def min_cluster_distances(c, D):
 
 
 def diameter(c, D):
-    """
-    Calculates cluster diameters (the distance between the two farthest data
+    """Calculates cluster diameters (the distance between the two farthest data
     points in a cluster)
     """
 
