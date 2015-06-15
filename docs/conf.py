1058a0d04a34268a21c5bb9886ae2f3756f137ce
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2015-06-15 01:39:58 -0300

    Still trying to fix RTD compilation

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 import shlex
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.optimize', 'scipy.cluster.hierarchy', 'numpy.random']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
