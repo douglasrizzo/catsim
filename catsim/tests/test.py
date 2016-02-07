4a47e06561169d438595032f7c73f1d0a7b749fc
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-07 01:19:48 -0200

    Revert "Revert "fixed small bugs so that tests can be ran successfully""
    This reverts commit ada6a835fac99033c42ea78e1a68368867eb2c19.

--- catsim/tests/test.py
+++ catsim/tests/test.py
@@ -1,10 +1,47 @@
 import unittest
+from catsim.cat import generate_item_bank
+from catsim import irt
+from catsim.initialization import RandomInitializer
+from catsim.selection import MaxInfoSelector
+from catsim.reestimation import HillClimbingEstimator, BinarySearchEstimator, DifferentialEvolutionEstimator, FMinEstimator
+from catsim.stopping import MaxItemStopper
+from catsim.simulation import Simulator
 
 
 class TestStuff(unittest.TestCase):
 
-    def nothing(self):
+    def item_bank_generation(self):
+        for items in [
+            generate_item_bank(5, '1PL'), generate_item_bank(5, '2PL'),
+            generate_item_bank(5, '3PL'), generate_item_bank(
+                5,
+                '3PL',
+                corr=0
+            )
+        ]:
+            irt.validate_item_bank(items, raise_err=True)
+
         self.assertTrue(True)
+
+    def simple_test_simulation(self):
+        initializer = RandomInitializer()
+        selector = MaxInfoSelector()
+        estimator = HillClimbingEstimator()
+        stopper = MaxItemStopper(20)
+        Simulator(generate_item_bank(100), 10).simulate(initializer, selector, estimator, stopper)
+
+    def different_estimators(self):
+        initializer = RandomInitializer()
+        selector = MaxInfoSelector()
+        stopper = MaxItemStopper(20)
+
+        for estimator in [
+            HillClimbingEstimator(), BinarySearchEstimator(),
+            DifferentialEvolutionEstimator((-8, 8)), FMinEstimator()
+        ]:
+            Simulator(
+                generate_item_bank(100), 10
+            ).simulate(initializer, selector, estimator, stopper)
 
 
 if __name__ == '__main__':
