2d9d9de2cbb111ceab87cb8493691cc3b320b06e
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-06 23:16:30 -0200

    removed unnecessary dependencies

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 
 from setuptools import setup, find_packages
 
-setup(name='catsim',
-      version='1.0',
-      description='Computerized Adaptive Testing' +
-                  ' assisted by Similarity Algorithm',
-      author='Douglas De Rizzo Meneghetti',
-      author_email='douglasrizzom@gmail.com',
-      url='https://www.github.com/douglasrizzo/catsim',
-      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-      install_requires=[
-          'numpy', 'scipy', 'matplotlib',
-          'scikit-learn', 'pandas', 'unidecode', 'pyzmq'],
-      license='GPLv2'
-      )
+setup(
+    name='catsim',
+    version='1.0',
+    description='Computerized Adaptive Testing' + ' assisted by Similarity Algorithm',
+    author='Douglas De Rizzo Meneghetti',
+    author_email='douglasrizzom@gmail.com',
+    url='https://www.github.com/douglasrizzo/catsim',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=[
+        'numpy', 'scipy', 'matplotlib', 'scikit-learn', 'unidecode', 'pyzmq'
+    ],
+    license='GPLv2'
+)
