9939d24a02db1122a176c6a840b7ee7c365eb7c8
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 04:58:43 -0200

    removed test folder, now there is only a top-level testing module

--- catsim/testing.py
+++ catsim/testing.py
@@ -1,6 +1,7 @@
 import unittest
 from catsim.cat import generate_item_bank
 from catsim import irt
+from catsim import plot
 from catsim.initialization import RandomInitializer
 from catsim.selection import MaxInfoSelector
 from catsim.reestimation import HillClimbingEstimator, BinarySearchEstimator, DifferentialEvolutionEstimator, FMinEstimator
@@ -10,7 +11,7 @@ from catsim.simulation import Simulator
 
 class TestStuff(unittest.TestCase):
 
-    def item_bank_generation(self):
+    def test_item_bank_generation(self):
         for items in [
             generate_item_bank(5, '1PL'), generate_item_bank(5, '2PL'),
             generate_item_bank(5, '3PL'), generate_item_bank(
@@ -21,16 +22,38 @@ class TestStuff(unittest.TestCase):
         ]:
             irt.validate_item_bank(items, raise_err=True)
 
-        self.assertTrue(True)
+            assert True
 
-    def simple_test_simulation(self):
+    def test_simple_simulation(self):
         initializer = RandomInitializer()
         selector = MaxInfoSelector()
         estimator = HillClimbingEstimator()
         stopper = MaxItemStopper(20)
         Simulator(generate_item_bank(100), 10).simulate(initializer, selector, estimator, stopper)
 
-    def different_estimators(self):
+        assert True
+
+    def test_plots(self):
+        initializer = RandomInitializer()
+        selector = MaxInfoSelector()
+        estimator = HillClimbingEstimator()
+        stopper = MaxItemStopper(20)
+        s = Simulator(generate_item_bank(100), 10)
+        s.simulate(initializer, selector, estimator, stopper)
+
+        a, b, c = s.items[0, 0:3]
+        plot.item_curve(a, b, c, title='Test plot', ptype='both', filepath='/tmp/irt.pdf')
+        plot.gen3D_dataset_scatter(items=s.items, filepath='/tmp/3d.pdf')
+        plot.test_progress(
+            title='Test progress',
+            simulator=s,
+            index=0,
+            filepath='/tmp/progress.pdf'
+        )
+
+        assert True
+
+    def test_different_estimators(self):
         initializer = RandomInitializer()
         selector = MaxInfoSelector()
         stopper = MaxItemStopper(20)
@@ -42,6 +65,8 @@ class TestStuff(unittest.TestCase):
             Simulator(
                 generate_item_bank(100), 10
             ).simulate(initializer, selector, estimator, stopper)
+
+        assert True
 
 
 if __name__ == '__main__':
