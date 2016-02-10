2da59d489c3540ac118cf9d95f929a601a783bbf
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 08:58:59 -0200

    fixed CISM

--- catsim/selection.py
+++ catsim/selection.py
@@ -1,5 +1,5 @@
 from catsim import irt
-import numpy as np
+import numpy
 from abc import ABCMeta, abstractmethod
 
 
@@ -11,7 +11,7 @@ class Selector:
         super(Selector, self).__init__()
 
     @abstractmethod
-    def select(self, items: np.ndarray, administered_items: list, est_theta: float) -> int:
+    def select(self, items: numpy.ndarray, administered_items: list, est_theta: float) -> int:
         """Get the indexes of all items that have not yet been administered, calculate
         their information value and pick the one with maximum information
 
@@ -30,7 +30,7 @@ class MaxInfoSelector(Selector):
     def __init__(self):
         super().__init__()
 
-    def select(self, items: np.ndarray, administered_items: list, est_theta: float) -> int:
+    def select(self, items: numpy.ndarray, administered_items: list, est_theta: float) -> int:
         """Get the indexes of all items that have not yet been administered, calculate
         their information value and pick the one with maximum information
 
@@ -40,9 +40,14 @@ class MaxInfoSelector(Selector):
         :param est_theta: estimated proficiency value
         :returns: index of the first non-administered item with maximum information
         """
-        valid_indexes = np.array(list(set(range(items.shape[0])) - set(administered_items)))
+        valid_indexes = numpy.array(list(set(range(items.shape[0])) - set(administered_items)))
         inf_values = [irt.inf(est_theta, i[0], i[1], i[2]) for i in items[valid_indexes]]
-        valid_indexes = [index for (inf_value, index) in sorted(zip(inf_values, valid_indexes), reverse=True)]
+        valid_indexes = [
+            index for (inf_value, index) in sorted(
+                zip(inf_values, valid_indexes),
+                reverse=True
+            )
+        ]
 
         return valid_indexes[0]
 
@@ -77,30 +82,41 @@ class ClusterSelector(Selector):
     :type r_control: string
     """
 
-    def __init__(self, clusters: list, method: str='item_info', r_max: float=1, r_control: str='passive'):
+    def __init__(
+        self,
+        clusters: list,
+        method: str='item_info',
+        r_max: float=1,
+        r_control: str='passive'
+    ):
         available_methods = ['item_info', 'cluster_info', 'weighted_info']
         if method not in available_methods:
-            raise ValueError('{0} is not a valid cluster selection method; choose one from {1}'.format(method, available_methods))
+            raise ValueError(
+                '{0} is not a valid cluster selection method; choose one from {1}'.format(
+                    method, available_methods
+                )
+            )
         available_rcontrol = ['passive', 'aggressive']
         if r_control not in available_rcontrol:
-            raise ValueError('{0} is not a valid item exposure control method; choose one from {1}'.format(r_control, available_rcontrol))
+            raise ValueError(
+                '{0} is not a valid item exposure control method; choose one from {1}'.format(
+                    r_control, available_rcontrol
+                )
+            )
 
         self.__clusters = clusters
         self.__method = method
         self.__r_max = r_max
         self.__r_control = r_control
 
-    def select(self, items: np.ndarray, administered_items: list, est_theta: float) -> int:
+    def select(self, items: numpy.ndarray, administered_items: list, est_theta: float) -> int:
         """CAT simulation and validation method proposed by [Bar10]_.
 
         :param items: an item matrix in which the first 3 columns represent item discrimination,
                       difficulty and pseudo-guessing parameters, respectively.
-        :type items: numpy.ndarray
         :param administered_items: a list with the indexes of all administered from the item matrix
         :param est_theta: estimated proficiency value
-        :type est_theta: float
         :returns: index of the first non-administered item with maximum information
-        :rtype: int
         """
         selected_cluster = None
         # this part of the code selects the cluster from which the item at
@@ -114,12 +130,22 @@ class ClusterSelector(Selector):
                     # gets the indexes of all the items in the same cluster
                     # as the current selected item that have not been
                     # administered
-                    valid_indexes = np.array(list(set(np.nonzero(items[:, 4] == i[4])[0]) - set(administered_items)))
+                    valid_indexes = numpy.array(
+                        list(
+                            set(
+                                numpy.nonzero(self.__clusters == self.__clusters[counter])[
+                                    0
+                                ]
+                            ) - set(
+                                administered_items
+                            )
+                        )
+                    )
 
                     # checks if at least one item from this cluster has not
                     # been adminitered to this examinee yet
                     if len(valid_indexes) > 0:
-                        selected_cluster = i[4]
+                        selected_cluster = self.__clusters[counter]
                         max_inf = irt.inf(est_theta, i[0], i[1], i[2])
 
         elif self.__method in ['cluster_info', 'weighted_info']:
@@ -128,16 +154,27 @@ class ClusterSelector(Selector):
             if self.__method == 'cluster_info':
                 cluster_infos = ClusterSelector.sum_cluster_infos(est_theta, items, self.__clusters)
             elif self.__method == 'weighted_info':
-                cluster_infos = ClusterSelector.weighted_cluster_infos(est_theta, items, self.__clusters)
+                cluster_infos = ClusterSelector.weighted_cluster_infos(
+                    est_theta, items, self.__clusters
+                )
 
             # sorts clusters descending by their information values
             # this type of sorting was seem on
             # http://stackoverflow.com/a/6618543
-            sorted_clusters = np.array([cluster for (inf_value, cluster) in sorted(zip(cluster_infos, set(self.__clusters)), reverse=True)], dtype=float)
+            sorted_clusters = numpy.array(
+                [
+                    cluster
+                    for (inf_value, cluster) in sorted(
+                        zip(cluster_infos, set(self.__clusters)),
+                        reverse=True
+                    )
+                ],
+                dtype=float
+            )
 
             # walks through the sorted clusters in order
             for i in range(len(sorted_clusters)):
-                valid_indexes = np.nonzero(items[:, 4] == sorted_clusters[i])[0]
+                valid_indexes = numpy.nonzero(items[:, 4] == sorted_clusters[i])[0]
 
                 # checks if at least one item from this cluster has not
                 # been adminitered to this examinee yet
@@ -156,18 +193,40 @@ class ClusterSelector(Selector):
 
         # gets the indexes and information values from the items in the
         # selected cluster that have not been administered
-        valid_indexes = np.array(list(set(np.nonzero(items[:, 4] == selected_cluster)[0]) - set(administered_items)))
+        valid_indexes = numpy.array(
+            list(
+                set(numpy.nonzero(self.__clusters == selected_cluster)[0]) - set(
+                    administered_items
+                )
+            )
+        )
 
         # gets the indexes and information values from the items in the
         # selected cluster with r < rmax that have not been
         # administered
-        valid_indexes_low_r = np.array(list(set(np.nonzero((items[:, 4] == selected_cluster) & (items[:, 3] < self.__r_max))[0]) - set(administered_items)))
+        valid_indexes_low_r = numpy.array(
+            list(
+                set(
+                    numpy.nonzero(
+                        (self.__clusters == selected_cluster) & (
+                            items[:, 3] < self.__r_max
+                        )
+                    )[0]
+                ) - set(administered_items)
+            )
+        )
 
         if len(valid_indexes_low_r) > 0:
             # sort both items and their indexes by their information
             # value
             inf_values = [irt.inf(est_theta, i[0], i[1], i[2]) for i in items[valid_indexes_low_r]]
-            valid_indexes_low_r = [index for (inf_value, index) in sorted(zip(inf_values, valid_indexes_low_r), reverse=True)]
+            valid_indexes_low_r = [
+                index
+                for (inf_value, index) in sorted(
+                    zip(inf_values, valid_indexes_low_r),
+                    reverse=True
+                )
+            ]
 
             selected_item = valid_indexes_low_r[0]
 
@@ -176,34 +235,44 @@ class ClusterSelector(Selector):
         else:
             if self.__r_control == 'passive':
                 inf_values = [irt.inf(est_theta, i[0], i[1], i[2]) for i in items[valid_indexes]]
-                valid_indexes = [index for (inf_value, index) in sorted(zip(inf_values, valid_indexes), reverse=True)]
+                valid_indexes = [
+                    index
+                    for (inf_value, index) in sorted(
+                        zip(inf_values, valid_indexes),
+                        reverse=True
+                    )
+                ]
             elif self.__r_control == 'aggressive':
-                valid_indexes = [index for (r, index) in sorted(zip(items[valid_indexes, 3], valid_indexes))]
+                valid_indexes = [
+                    index for (r, index) in sorted(zip(items[valid_indexes, 3], valid_indexes))
+                ]
 
             selected_item = valid_indexes[0]
 
         return selected_item
 
     @staticmethod
-    def sum_cluster_infos(theta: float, items: np.ndarray, clusters: list) -> list:
+    def sum_cluster_infos(theta: float, items: numpy.ndarray, clusters: list) -> list:
         """Returns the sum of item informations, separated by cluster
 
         :param theta: an examinee's :math:`\\theta` value
         :param items: a matrix containing item parameters
         :param clusters: a list containing item cluster memberships, represented by integers
         :returns: list containing the sum of item information values for each cluster"""
-        cluster_infos = np.zeros((len(set(clusters))))
+        cluster_infos = numpy.zeros((len(set(clusters))))
 
         for cluster in set(clusters):
-            cluster_indexes = np.nonzero(clusters == cluster)[0]
+            cluster_indexes = numpy.nonzero(clusters == cluster)[0]
 
             for item in items[cluster_indexes]:
-                cluster_infos[cluster] = cluster_infos[cluster] + irt.inf(theta, item[0], item[1], item[2])
+                cluster_infos[cluster] = cluster_infos[cluster] + irt.inf(
+                    theta, item[0], item[1], item[2]
+                )
 
         return cluster_infos
 
     @staticmethod
-    def weighted_cluster_infos(theta: float, items: np.ndarray, clusters: list):
+    def weighted_cluster_infos(theta: float, items: numpy.ndarray, clusters: list):
         """Returns the weighted sum of item informations, separated by cluster.
         The weight is the number of items in each cluster.
 
@@ -213,9 +282,45 @@ class ClusterSelector(Selector):
         :returns: list containing the sum of item information values for each cluster,
                  divided by the number of items in each cluster"""
         cluster_infos = ClusterSelector.sum_cluster_infos(theta, items, clusters)
-        count = np.bincount(clusters)
+        count = numpy.bincount(clusters)
 
         for i in range(len(cluster_infos)):
             cluster_infos[i] = cluster_infos[i] / count[i]
 
         return cluster_infos
+
+    @staticmethod
+    def cluster_sum(items: numpy.ndarray, c: list):
+        """Returns the sum of item parameter values for each cluster cluster
+
+        :param items: a matrix containing item parameters.
+        :param c: a list containing clustering memeberships.
+        :returns: a matrix containing the sum of each parameter by cluster. Lines are clusters, columns are parameters."""
+        averages = numpy.zeros((numpy.max(c) + 1, 3))
+
+        for i in numpy.arange(0, numpy.size(c)):
+            if c[i] == -1:
+                continue
+            averages[c[i], 0] += items[i, 0]
+            averages[c[i], 1] += items[i, 1]
+            averages[c[i], 2] += items[i, 2]
+
+        return averages
+
+    @staticmethod
+    def cluster_averages(items: numpy.ndarray, c: list):
+        """Returns the average values of item parameters by cluster
+
+        :param items: a matrix containing item parameters.
+        :param c: a list containing clustering memeberships.
+        :returns: a matrix containing the average values of each parameter by cluster. Lines are clusters, columns are parameters."""
+        averages = ClusterSelector.cluster_sum(items, c)
+
+        occurrences = numpy.bincount(numpy.delete(c, numpy.where(c == -1)).astype(numpy.int64))
+
+        for counter, i in enumerate(occurrences):
+            averages[counter, 0] = averages[counter, 0] / i
+            averages[counter, 1] = averages[counter, 1] / i
+            averages[counter, 2] = averages[counter, 2] / i
+
+        return averages
