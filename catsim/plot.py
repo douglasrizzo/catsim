3f0ddea8bf7ba0478caf58f70f1f437c1851764b
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 05:06:47 -0200

    plots are now working

--- catsim/plot.py
+++ catsim/plot.py
@@ -2,8 +2,10 @@
 
 import os
 from catsim import irt
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
+from catsim.simulation import Simulator
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def __column(matrix, i):
@@ -11,29 +13,33 @@ def __column(matrix, i):
     return [row[i] for row in matrix]
 
 
-def plot_irt(a=1, b=0, c=0, title=None, ptype='icc', filepath=None):
+def item_curve(
+    a: float=1,
+    b: float=0,
+    c: float=0,
+    title: str=None,
+    ptype: str='icc',
+    filepath: str=None
+):
     """Plots 'Item Response Theory'-related item plots
 
     :param a: item discrimination parameter
-    :type a: float
     :param b: item difficulty parameter
-    :type b: float
     :param c: item pseudo-guessing parameter
-    :type c: float
     :param title: plot title
-    :type title: string
     :param ptype: 'icc' for the item characteristic curve, 'iif' for the item
                   information curve or 'both' for both curves in the same plot
-    :type ptype: string
     :param filepath: saves the plot in the given path
-    :type filepath: string
     """
     available_types = ['icc', 'iif', 'both']
+
+    if not os.path.exists(os.path.dirname(filepath)):
+        os.makedirs(os.path.dirname(filepath))
 
     if ptype not in available_types:
         raise ValueError('\'{0}\' not in available plot types: {1}'.format(ptype, available_types))
 
-    thetas = np.arange(b - 4, b + 4, .1, 'double')
+    thetas = numpy.arange(b - 4, b + 4, .1, 'double')
     p_thetas = []
     i_thetas = []
     for theta in thetas:
@@ -42,8 +48,6 @@ def plot_irt(a=1, b=0, c=0, title=None, ptype='icc', filepath=None):
 
     if ptype in ['icc', 'iif']:
         plt.figure()
-        if title is not None:
-            plt.title(title, size=18)
         plt.annotate(
             '$a = ' + format(a) + '$\n$b = ' + format(
                 b
@@ -62,10 +66,16 @@ def plot_irt(a=1, b=0, c=0, title=None, ptype='icc', filepath=None):
         if ptype == 'icc':
             plt.ylabel(r'$P(\theta)$')
             plt.plot(thetas, p_thetas)
+            if title is not None:
+                title = 'Item characteristic curve'
 
         elif ptype == 'iif':
             plt.ylabel(r'$I(\theta)$')
             plt.plot(thetas, i_thetas)
+            if title is not None:
+                title = 'Item information curve'
+
+        plt.title(title, size=18)
 
     elif ptype == 'both':
         fig, ax1 = plt.subplots()
@@ -83,7 +93,8 @@ def plot_irt(a=1, b=0, c=0, title=None, ptype='icc', filepath=None):
         for tl in ax2.get_yticklabels():
             tl.set_color('r')
 
-        ax1.set_title(title, size=18)
+        if title is not None:
+            ax1.set_title(title, size=18)
 
         ax2.annotate(
             '$a = ' + format(a) + '$\n$b = ' + format(
@@ -99,32 +110,101 @@ def plot_irt(a=1, b=0, c=0, title=None, ptype='icc', filepath=None):
         ax2.legend(loc='best', framealpha=0)
 
     if filepath is not None:
-        plt.savefig(filepath)
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath))
+        plt.savefig(filepath, bbox_inches='tight')
 
+    plt.show()
     plt.close()
 
 
-def gen3D_dataset_scatter(title, items, path):
+def gen3D_dataset_scatter(items: numpy.ndarray, title: str='Item parameters', filepath: str=None):
     """Generate the item matrix tridimensional dataset scatter plot
 
-    :param title: the scatter plot title, which will also be used as the file name
-    :type title: string
     :param items: the item matrix
-    :type items: numpy.ndarray
-    :param path: the path to save the scatter plot
-    :type path: string
+    :param title: the scatter plot title
+    :param filepath: the path to save the scatter plot
     """
     irt.validate_item_bank(items)
 
-    if not os.path.exists(path):
-        os.makedirs(path)
-
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(__column(items.tolist(), 0), __column(items.tolist(), 1), __column(items.tolist(), 2), s=10)
-    ax.set_title(title)
+    ax.scatter(
+        __column(items.tolist(), 0),
+        __column(items.tolist(), 1),
+        __column(items.tolist(), 2),
+        s=10
+    )
+
+    plt.title(title, size=18)
+
     ax.set_xlabel('a')
     ax.set_ylabel('b')
     ax.set_zlabel('c')
 
-    plt.savefig(path + title + '.pdf', bbox_inches='tight')
+    if filepath is not None:
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath))
+        plt.savefig(filepath, bbox_inches='tight')
+
+    plt.show()
+    plt.close()
+
+
+def test_progress(
+    title: str=None,
+    simulator: Simulator=None,
+    index: int=None,
+    thetas: list=None,
+    administered_items: numpy.ndarray=None,
+    true_theta: float=None,
+    filepath: str=None
+):
+    """Generates a plot representing an examinee's test progress
+
+    :param title: the plot title
+    :param simulator: a simulator which has already simulated a series of CATs, containing estimations to the examinees' proficiencies and a list of administered items for each examinee
+    :param index: the index of the examinee in the simulator whose plot is to be done
+    :param thetas: if a :py:class:`Simulator` is not passed, then a list of proficiency estimations can be manually passed to the function
+    :param administered_items: if a :py:class:`Simulator` is not passed, then a matrix of administered items, represented by their parameters, can be manually passed to the function
+    :param true_theta: the value of the examinee's true proficiency. If it is passed, it will be shown on the plot, otherwise not
+    :param filepath: the path to save the plot
+    """
+    if simulator is None and thetas is None and administered_items is None:
+        raise ValueError('Not a single plottable object was passed.')
+
+    plt.figure()
+    plt.title(title, size=18)
+    if simulator is not None and index is not None:
+        thetas = simulator.estimations[index]
+        administered_items = simulator.items[simulator.administered_items[index]]
+        true_theta = simulator.examinees[index]
+
+    if thetas is not None and administered_items is not None and len(thetas) != len(
+        administered_items[:, 1]
+    ):
+        raise ValueError(
+            'Number of estimations and administered items is not the same. They should be.'
+        )
+
+    xs = range(len(thetas)) if thetas is not None else range(len(administered_items[:, 1]))
+
+    if thetas is not None:
+        plt.plot(xs, thetas, label=r'$\theta$')
+    if administered_items is not None:
+        difficulties = administered_items[:, 1]
+        plt.plot(xs, difficulties, label='Item difficulty')
+    if true_theta is not None:
+        plt.hlines(true_theta, 0, len(xs), label='True theta')
+
+    plt.xlabel('Items')
+    plt.grid()
+    plt.legend(loc='best')
+
+    if filepath is not None:
+        if not os.path.exists(os.path.dirname(filepath)):
+            os.makedirs(os.path.dirname(filepath))
+        plt.savefig(filepath, bbox_inches='tight')
+
+    plt.show()
+    plt.close()
