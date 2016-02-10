7dff992caffecbf5d72f27082634c2304f56d2b3
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 05:02:30 -0200

    included function annotations

--- catsim/reestimation.py
+++ catsim/reestimation.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 
 from catsim import irt
 import numpy
+import pymc
 from scipy.optimize import fmin
 from scipy.optimize import differential_evolution
 
@@ -15,20 +16,18 @@ class Estimator:
         super(Estimator, self).__init__()
 
     @abstractmethod
-    def estimate(self, response_vector, administered_items, current_theta):
-        """Uses a hill-climbing algorithm to find and returns the theta value
+    def estimate(
+        self, response_vector: list, administered_items: numpy.ndarray, current_theta: float
+    ) -> float:
+        """Uses a hill-climbing algorithm to find and return the theta value
         that maximizes the likelihood function, given a response vector and a
         matrix with the administered items parameters.
 
         :param response_vector: a binary list containing the response vector
-        :type response_vector: list
         :param administered_items: a matrix containing the parameters of the
                                    answered items
-        :type administered_items: numpy.ndarray
         :param current_theta: the current estimation of the examinee's :math:`\\theta` value
-        :type current_theta: float
         :returns: a new estimation of the examinee's proficiency, given his answers up until now
-        :rtype: float
         """
         pass
 
@@ -37,12 +36,10 @@ class HillClimbingEstimator(Estimator):
     """Estimator that uses a hill-climbing algorithm to maximize the likelihood function
 
     :param precision: number of decimal points of precision
-    :type precision: int
     :param verbose: verbosity level of the maximization method
-    :type verbose: boolean
     """
 
-    def __init__(self, precision=6, verbose=False):
+    def __init__(self, precision: int=6, verbose: bool=False):
         super(HillClimbingEstimator, self).__init__()
         self.__precision = precision
         self.__verbose = verbose
@@ -52,18 +49,15 @@ class HillClimbingEstimator(Estimator):
         response_vector: list,
         administered_items: numpy.ndarray,
         current_theta: float=None
-    ):
+    ) -> float:
         """Uses a hill-climbing algorithm to find and returns the theta value
         that maximizes the likelihood function, given a response vector and a
         matrix with the administered items parameters.
 
         :param response_vector: a binary list containing the response vector
-        :type response_vector: list
         :param administered_items: a matrix containing the parameters of the
                                    answered items
-        :type administered_items: numpy.ndarray
-        :param current_theta: the current estimation of the examinee's :math:`\\theta` value; not used by this selector.
-        :type current_theta: float
+        :param current_theta: not used by this selector
         :returns: a new estimation of the examinee's proficiency, given his answers up until now
         :rtype: float
         """
@@ -122,7 +116,12 @@ class BinarySearchEstimator(Estimator):
         self.__precision = precision
         self.__verbose = verbose
 
-    def estimate(self, response_vector: list, administered_items: numpy.ndarray) -> float:
+    def estimate(
+        self,
+        response_vector: list,
+        administered_items: numpy.ndarray,
+        current_theta: float=None
+    ) -> float:
         """Uses a binary search algorithm to find and returns the theta value
         that maximizes the likelihood function, given a response vector and a
         matrix with the administered items parameters.
@@ -130,6 +129,7 @@ class BinarySearchEstimator(Estimator):
         :param response_vector: a binary list containing the response vector
         :param administered_items: a matrix containing the parameters of the
                                    answered items
+        :param current_theta: not used by this selector
         :returns: a new estimation of the examinee's proficiency, given his answers up until now
         """
 
@@ -145,7 +145,7 @@ class BinarySearchEstimator(Estimator):
 
         while True:
             iters += 1
-            if self.verbose:
+            if self.__verbose:
                 print('Bounds: ' + str(lbound) + ' ' + str(ubound))
                 print(
                     'Iteration: {0}, Theta: {1}, LL: {2}'.format(
@@ -159,14 +159,14 @@ class BinarySearchEstimator(Estimator):
                 lbound, response_vector, administered_items
             ):
 
-                if abs(best_theta - ubound) < float('1e-' + str(self.precision)):
+                if abs(best_theta - ubound) < float('1e-' + str(self.__precision)):
                     return ubound
 
                 best_theta = ubound
                 lbound += (ubound - lbound) / 2
             else:
 
-                if abs(best_theta - lbound) < float('1e-' + str(self.precision)):
+                if abs(best_theta - lbound) < float('1e-' + str(self.__precision)):
                     return lbound
 
                 best_theta = lbound
@@ -179,20 +179,19 @@ class FMinEstimator(Estimator):
     def __init__(self):
         super(FMinEstimator, self).__init__()
 
-    def estimate(response_vector, administered_items, current_theta):
+    def estimate(
+        self, response_vector: list, administered_items: numpy.ndarray, current_theta: float
+    ) -> float:
         """Uses :py:func:`scipy.optimize.fmin` to find and return the theta value
         that minimizes the negative log-likelihood function, given a response vector, a
         matrix with the administered items parameters and the current :math:`\\theta`.
 
         :param response_vector: a binary list containing the response vector
-        :type response_vector: list
         :param administered_items: a matrix containing the parameters of the
                                    answered items
         :type administered_items: numpy.ndarray
         :param current_theta: the current estimation of the examinee's :math:`\\theta` value
-        :type current_theta: float
         :returns: a new estimation of the examinee's proficiency, given his answers up until now
-        :rtype: float
         """
         return fmin(irt.negativelogLik, current_theta, (response_vector, administered_items))
 
@@ -200,36 +199,33 @@ class FMinEstimator(Estimator):
 class DifferentialEvolutionEstimator(Estimator):
     """Estimator that uses :py:func:`scipy.optimize.differential_evolution` to minimize the negative log-likelihood function
 
-    :param lower_bound: a lower bound for the differential evolution algorithm search space.
-                           In theory, it is best if it represents the minimum possible
-                           :math:`\\theta` value; in practice, one could also use the smallest
-                           difficulty parameter of an item in the item bank, in case no better
-                           lower bound for :math:`\\theta` exists.
-    :type lower_bound: float
-    :param upper_bound: an upper bound for the differential evolution algorithm search space.
-                           In theory, it is best if it represents the maximum possible
-                           :math:`\\theta` value; in practice, one could also use the largest
-                           difficulty parameter of an item in the item bank, in case no better
-                           upper bound for :math:`\\theta` exists.
-    :type upper_bound: float
+    :param bounds: a tuple containing both lower and upper bounds for the differential evolution algorithm search space.
+                   In theory, it is best if they represent the minimum and maximum possible
+                   :math:`\\theta` values; in practice, one could also use the smallest and largest
+                   difficulty parameters in the item bank, in case no better
+                   bounds for :math:`\\theta` exist.
     """
 
-    def __init__(self, lower_bound, upper_bound):
+    def __init__(self, bounds: tuple):
         super(DifferentialEvolutionEstimator, self).__init__()
-        self.__lower_bound = lower_bound
-        self.__upper_bound = upper_bound
+        self.__lower_bound = min(bounds)
+        self.__upper_bound = max(bounds)
 
-    def estimate(self, response_vector, administered_items, current_theta):
+    def estimate(
+        self,
+        response_vector: list,
+        administered_items: numpy.ndarray,
+        current_theta: float=None
+    ) -> float:
         """Uses :py:func:`scipy.optimize.fmin` to find and return the theta value
         that minimizes the negative log-likelihood function, given a response vector, a
         matrix with the administered items parameters and the current :math:`\\theta`.
 
         :param response_vector: a binary list containing the response vector
-        :type response_vector: list
         :param administered_items: a matrix containing the parameters of the
                                    answered items
-        :type administered_items: numpy.ndarray
-        :returns: a new estimation of the examinee's proficiency, given his answers up until now
+        :param current_theta: the current estimation of the examinee's :math:`\\theta` value
+        :returns: not used by this selector
         :rtype: float
         """
 
@@ -240,3 +236,39 @@ class DifferentialEvolutionEstimator(Estimator):
             ],
             args=(response_vector, administered_items)
         ).x[0]
+
+# class BayesianEstimator(Estimator):
+#     """Bayesian estimator for maximizing the likelihood function
+#
+#     .. math:: P(\\theta_j | X_{Ij}, a_I, b_I, c_I) = P(X_{Ij} | \\theta_j, a_I, b_I, c_I)P(\\theta_j|\\eta)
+#
+#     """
+#
+#     def __init__(self):
+#         super(BayesianEstimator, self).__init__()
+#
+#     def estimate(
+#         self,
+#         response_vector: list,
+#         administered_items: numpy.ndarray,
+#         current_theta: float=None
+#     ):
+#         pass
+#
+#     def model(thetas, administered_items):
+#         # I think this is how I pass the parameters in administered_items to PyMC
+#         a = pymc.Normal("a", mu=1, tau=1, value=administered_items[:, 0])
+#         b = pymc.Normal("b", mu=0, tau=1, value=administered_items[:, 1])
+#         c = pymc.Normal("c", mu=.25, tau=.02, value=administered_items[:, 2])
+#
+#         theta_prior = pymc.Normal('theta_prior', mu=0.0, tau=1.0)
+#         answers = pymc.Bernoulli('answers', p=irt.tpm, value=thetas, observed=True)
+#
+#         mod = pymc.Model([theta_prior, answers, a, b, c])
+#         mc = pymc.MCMC(mod)
+#         mc.sample(iter=5000, burn=1000)
+#         pymc.Matplot.histogram(
+#             mc.trace('theta_prior')[:],
+#             "theta prior; size=100",
+#             datarange=(0.2, 0.9)
+#         )
