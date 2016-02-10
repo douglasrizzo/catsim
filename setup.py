9f4644e37aea97335121e75ae89334ddf957b6b6
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-10 04:59:32 -0200

    added config file for Pypi

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 setup(
     name='catsim',
     version='1.2a',
-    description='Computerized Adaptive Testing' + ' assisted by Similarity Algorithm',
+    description='Computerized Adaptive Testing Simulator',
     author='Douglas De Rizzo Meneghetti',
     author_email='douglasrizzom@gmail.com',
     url='https://www.github.com/douglasrizzo/catsim',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
-        'numpy', 'scipy', 'matplotlib', 'scikit-learn', 'unidecode', 'pyzmq'
+        'numpy', 'scipy', 'matplotlib', 'scikit-learn', 'pyzmq'
     ],
     license='GPLv2'
 )
