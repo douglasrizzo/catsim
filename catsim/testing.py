965f7410eb24dd8a635d41bdecbe639a65f99fb6
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-12 04:14:14 -0200

    test SEE stopping criterion

--- catsim/testing.py
+++ catsim/testing.py
@@ -5,7 +5,7 @@ from catsim import plot
 from catsim.initialization import RandomInitializer
 from catsim.selection import MaxInfoSelector, ClusterSelector
 from catsim.reestimation import HillClimbingEstimator, BinarySearchEstimator, DifferentialEvolutionEstimator, FMinEstimator
-from catsim.stopping import MaxItemStopper
+from catsim.stopping import MaxItemStopper, MinErrorStopper
 from catsim.simulation import Simulator
 
 
@@ -24,47 +24,40 @@ class TestStuff(unittest.TestCase):
 
             assert True
 
-    def test_simple_simulation(self):
+    # def test_plots(self):
+    #     initializer = RandomInitializer()
+    #     selector = MaxInfoSelector()
+    #     estimator = HillClimbingEstimator()
+    #     stopper = MaxItemStopper(20)
+    #     s = Simulator(generate_item_bank(100), 10)
+    #     s.simulate(initializer, selector, estimator, stopper)
+    #
+    #     a, b, c = s.items[0, 0:3]
+    #     plot.item_curve(a, b, c, title='Test plot', ptype='both', filepath='/tmp/irt.pdf')
+    #     plot.gen3D_dataset_scatter(items=s.items, filepath='/tmp/3d.pdf')
+    #     plot.test_progress(
+    #         title='Test progress',
+    #         simulator=s,
+    #         index=0,
+    #         filepath='/tmp/progress.pdf'
+    #     )
+    #
+    #     assert True
+
+    def test_simulations(self):
+        items = generate_item_bank(5000)
+
         initializer = RandomInitializer()
         selector = MaxInfoSelector()
-        estimator = HillClimbingEstimator()
-        stopper = MaxItemStopper(20)
-        Simulator(generate_item_bank(100), 10).simulate(initializer, selector, estimator, stopper)
-
-        assert True
-
-    def test_plots(self):
-        initializer = RandomInitializer()
-        selector = MaxInfoSelector()
-        estimator = HillClimbingEstimator()
-        stopper = MaxItemStopper(20)
-        s = Simulator(generate_item_bank(100), 10)
-        s.simulate(initializer, selector, estimator, stopper)
-
-        a, b, c = s.items[0, 0:3]
-        plot.item_curve(a, b, c, title='Test plot', ptype='both', filepath='/tmp/irt.pdf')
-        plot.gen3D_dataset_scatter(items=s.items, filepath='/tmp/3d.pdf')
-        plot.test_progress(
-            title='Test progress',
-            simulator=s,
-            index=0,
-            filepath='/tmp/progress.pdf'
-        )
-
-        assert True
-
-    def test_different_estimators(self):
-        initializer = RandomInitializer()
-        selector = MaxInfoSelector()
-        stopper = MaxItemStopper(20)
 
         for estimator in [
             HillClimbingEstimator(), BinarySearchEstimator(),
             DifferentialEvolutionEstimator((-8, 8)), FMinEstimator()
         ]:
-            Simulator(
-                generate_item_bank(100), 10
-            ).simulate(initializer, selector, estimator, stopper)
+            for stopper in [MaxItemStopper(20), MinErrorStopper(.4)]:
+                s = Simulator(items, 10)
+                s.simulate(initializer, selector, estimator, stopper)
+                # plot.test_progress(simulator=s, index=0, see=True)
 
         assert True
 
@@ -80,9 +73,6 @@ class TestStuff(unittest.TestCase):
         stopper = MaxItemStopper(20)
         simulator = Simulator(items, 10)
         simulator.simulate(initializer, selector, estimator, stopper)
-
-        plot.test_progress(simulator=simulator, index=0)
-        plot.test_progress(simulator=simulator, index=len(simulator.examinees) - 1)
 
         assert True
 
