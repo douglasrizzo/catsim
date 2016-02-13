6c34a78440af585d3c4ac65923f7b25c670565d9
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-13 04:52:07 -0200

    removed matplotlib from tests

--- catsim/testing.py
+++ catsim/testing.py
@@ -1,7 +1,6 @@
 import unittest
 from catsim.cat import generate_item_bank
 from catsim import irt
-from catsim import plot
 from catsim.initialization import RandomInitializer
 from catsim.selection import MaxInfoSelector, ClusterSelector
 from catsim.reestimation import HillClimbingEstimator, BinarySearchEstimator, DifferentialEvolutionEstimator, FMinEstimator
@@ -23,26 +22,6 @@ class TestStuff(unittest.TestCase):
             irt.validate_item_bank(items, raise_err=True)
 
             assert True
-
-    # def test_plots(self):
-    #     initializer = RandomInitializer()
-    #     selector = MaxInfoSelector()
-    #     estimator = HillClimbingEstimator()
-    #     stopper = MaxItemStopper(20)
-    #     s = Simulator(generate_item_bank(100), 10)
-    #     s.simulate(initializer, selector, estimator, stopper)
-    #
-    #     a, b, c = s.items[0, 0:3]
-    #     plot.item_curve(a, b, c, title='Test plot', ptype='both', filepath='/tmp/irt.pdf')
-    #     plot.gen3D_dataset_scatter(items=s.items, filepath='/tmp/3d.pdf')
-    #     plot.test_progress(
-    #         title='Test progress',
-    #         simulator=s,
-    #         index=0,
-    #         filepath='/tmp/progress.pdf'
-    #     )
-    #
-    #     assert True
 
     def test_simulations(self):
         items = generate_item_bank(5000)
