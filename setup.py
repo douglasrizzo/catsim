a07da507ceffaab2ec1f66670a3368dc8a54c78b
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2016-02-18 02:50:29 -0200

    changed version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Topic :: Education :: Testing'
     ],
     name='catsim',
-    version='0.8.2',
+    version='0.8.4',
     description='Computerized Adaptive Testing Simulator',
     author='Douglas De Rizzo Meneghetti',
     author_email='douglasrizzom@gmail.com',
