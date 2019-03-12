2b5f2b95cee68171d22e6056bf4add14ec6db435
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-03-12 15:33:01 -0300

    fix documentation...

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,10 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-<<<<<<< HEAD
     VERSION: '0.15.3',
-=======
-    VERSION: '0.15.2',
->>>>>>> testing
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
