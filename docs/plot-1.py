196f1ea2d125cb64a2899cb1c008cc217ecc6422
Douglas <douglasrizzom@gmail.com>
2021-06-26 06:40:26 -0300

    update docs

--- docs/plot-1.py
+++ docs/plot-1.py
@@ -1,4 +1,5 @@
-from catsim.cat import generate_item_bank
 from catsim import plot
+from catsim.cat import generate_item_bank
+
 items = generate_item_bank(100)
 plot.gen3d_dataset_scatter(items)
--- docs/plot-2.py
+++ docs/plot-2.py
@@ -1,6 +1,7 @@
-from catsim.cat import generate_item_bank
 from catsim import plot
+from catsim.cat import generate_item_bank
+
 item = generate_item_bank(1)[0]
-plot.item_curve(item[0], item[1], item[2], item[3], ptype='icc')
-plot.item_curve(item[0], item[1], item[2], item[3], ptype='iic')
-plot.item_curve(item[0], item[1], item[2], item[3], ptype='both')
+plot.item_curve(item[0], item[1], item[2], item[3], ptype="icc")
+plot.item_curve(item[0], item[1], item[2], item[3], ptype="iic")
+plot.item_curve(item[0], item[1], item[2], item[3], ptype="both")
