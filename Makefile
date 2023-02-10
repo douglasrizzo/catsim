0a4b25db049d2a4156c2c3f3efc572ce7dffc2fc
Douglas <douglasrizzom@gmail.com>
2023-02-09 22:57:19 -0300

    replace nose with pytest

--- Makefile
+++ Makefile
@@ -7,12 +7,13 @@ tests:
 	pip install .[testing]
 	flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 	flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
-	nosetests -s --cov-config .coveragerc --with-coverage --cover-package=catsim
+	pytest
 upload-test: dist
 	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 upload: dist
 	twine upload dist/*
 format:
+	autoflake .
 	isort .
 	black .
 	yapf -i -r .
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,27 @@
 import random
+
+import numpy as np
+import pytest
 from sklearn.cluster import KMeans
 
-from catsim import plot
+from catsim import cat, irt, plot
 from catsim.cat import generate_item_bank
-from catsim.estimation import *
-from catsim.initialization import *
-from catsim.selection import *
-from catsim.simulation import Simulator
+from catsim.estimation import NumericalSearchEstimator
+from catsim.initialization import FixedPointInitializer, RandomInitializer
+from catsim.selection import (
+    AStratBBlockSelector,
+    AStratSelector,
+    ClusterSelector,
+    LinearSelector,
+    MaxInfoBBlockSelector,
+    MaxInfoSelector,
+    MaxInfoStratSelector,
+    RandomesqueSelector,
+    RandomSelector,
+    The54321Selector,
+    UrrySelector,
+)
+from catsim.simulation import Estimator, Initializer, Selector, Simulator, Stopper
 from catsim.stopping import MaxItemStopper, MinErrorStopper
 
 
@@ -16,106 +31,147 @@ def one_simulation(items, examinees, initializer, selector, estimator, stopper):
     cat.rmse(s.examinees, s.latest_estimations)
 
 
-def test_cism():
-    examinees = 100
-    test_sizes = [30]
-    bank_sizes = [500]
-
-    for bank_size in bank_sizes:
-        for test_size in test_sizes:
-
-            initializers = [RandomInitializer("uniform", (-5, 5))]
-            estimators = [NumericalSearchEstimator()]
-            stoppers = [MaxItemStopper(test_size), MinErrorStopper(0.4)]
-
-            for initializer in initializers:
-                for estimator in estimators:
-                    for stopper in stoppers:
-                        items = generate_item_bank(bank_size)
-                        clusters = list(KMeans(n_clusters=8).fit_predict(items))
-                        ClusterSelector.weighted_cluster_infos(0, items, clusters)
-                        ClusterSelector.avg_cluster_params(items, clusters)
-                        selector = ClusterSelector(clusters=clusters, r_max=0.2)
-                        yield one_simulation, items, examinees, initializer, selector, estimator, stopper
+@pytest.mark.parametrize("examinees", [100])
+@pytest.mark.parametrize("bank_size", [500])
+@pytest.mark.parametrize("initializer", [RandomInitializer("uniform", (-5, 5))])
+@pytest.mark.parametrize("estimator", [NumericalSearchEstimator()])
+@pytest.mark.parametrize("stopper", [MaxItemStopper(30), MinErrorStopper(0.4)])
+def test_cism(
+    examinees: int,
+    bank_size: int,
+    initializer: Initializer,
+    estimator: Estimator,
+    stopper: Stopper,
+):
+    items = generate_item_bank(bank_size)
+    clusters = list(KMeans(n_clusters=8).fit_predict(items))
+    ClusterSelector.weighted_cluster_infos(0, items, clusters)
+    ClusterSelector.avg_cluster_params(items, clusters)
+    selector = ClusterSelector(clusters=clusters, r_max=0.2)
+    one_simulation(items, examinees, initializer, selector, estimator, stopper)
 
 
-def test_simulations():
-    examinees = 100
-    test_sizes = [30]
-    bank_sizes = [500]
+@pytest.mark.parametrize("examinees", [100])
+@pytest.mark.parametrize("test_size", [30])
+@pytest.mark.parametrize("bank_size", [500])
+@pytest.mark.parametrize("logistic_model", ["4PL"])
+@pytest.mark.parametrize(
+    "initializer",
+    [
+        RandomInitializer("uniform", (-5, 5)),
+        FixedPointInitializer(0), ],
+)
+@pytest.mark.parametrize(
+    "estimator", [NumericalSearchEstimator(method=m) for m in NumericalSearchEstimator.methods])
+def test_finite_selectors(
+    examinees: int,
+    test_size: int,
+    bank_size: int,
+    logistic_model: str,
+    initializer: Initializer,
+    estimator: Estimator,
+):
+    finite_selectors = [
+        LinearSelector(list(np.random.choice(bank_size, size=test_size, replace=False))),
+        AStratSelector(test_size),
+        AStratBBlockSelector(test_size),
+        MaxInfoStratSelector(test_size),
+        MaxInfoBBlockSelector(test_size),
+        The54321Selector(test_size),
+        RandomesqueSelector(test_size / 6), ]
+    stopper = MaxItemStopper(test_size)
 
-    logistic_models = ["4PL"]
+    for selector in finite_selectors:
+        items = generate_item_bank(bank_size, itemtype=logistic_model)
+        responses = cat.random_response_vector(random.randint(1, test_size - 1))
+        administered_items = np.random.choice(bank_size, len(responses), replace=False)
+        est_theta = initializer.initialize()
+        selector.select(
+            items=items,
+            administered_items=administered_items,
+            est_theta=est_theta,
+        )
+        estimator.estimate(
+            items=items,
+            administered_items=administered_items,
+            response_vector=responses,
+            est_theta=est_theta,
+        )
+        stopper.stop(
+            administered_items=items[administered_items],
+            theta=est_theta,
+        )
 
-    for bank_size in bank_sizes:
-        for test_size in test_sizes:
-            initializers = [
-                RandomInitializer("uniform", (-5, 5)),
-                FixedPointInitializer(0),
-            ]
-            infinite_selectors = [MaxInfoSelector(), RandomSelector(), UrrySelector()]
-            finite_selectors = [
-                LinearSelector(list(numpy.random.choice(bank_size, size=test_size, replace=False))),
-                AStratSelector(test_size),
-                AStratBBlockSelector(test_size),
-                MaxInfoStratSelector(test_size),
-                MaxInfoBBlockSelector(test_size),
-                The54321Selector(test_size),
-                RandomesqueSelector(5),
-            ]
+        one_simulation(items, examinees, initializer, selector, estimator, stopper)
 
-            for logistic_model in logistic_models:
-                for initializer in [FixedPointInitializer(0)]:
-                    for estimator in [
-                        NumericalSearchEstimator(method=m) for m in NumericalSearchEstimator.methods
-                    ]:
-                        for stopper in [MaxItemStopper(test_size)]:
-                            for selector in finite_selectors:
-                                items = generate_item_bank(bank_size, itemtype=logistic_model)
-                                responses = cat.random_response_vector(
-                                    random.randint(1, test_size - 1)
-                                )
-                                administered_items = numpy.random.choice(
-                                    bank_size, len(responses), replace=False
-                                )
-                                est_theta = initializer.initialize()
-                                selector.select(
-                                    items=items,
-                                    administered_items=administered_items,
-                                    est_theta=est_theta,
-                                )
-                                estimator.estimate(
-                                    items=items,
-                                    administered_items=administered_items,
-                                    response_vector=responses,
-                                    est_theta=est_theta,
-                                )
-                                stopper.stop(
-                                    administered_items=items[administered_items],
-                                    theta=est_theta,
-                                )
 
-                                yield one_simulation, items, examinees, initializer, selector, estimator, stopper
+@pytest.mark.parametrize("examinees", [100])
+@pytest.mark.parametrize("test_size", [30])
+@pytest.mark.parametrize("bank_size", [500])
+@pytest.mark.parametrize("logistic_model", ["4PL"])
+@pytest.mark.parametrize(
+    "initializer",
+    [
+        RandomInitializer("uniform", (-5, 5)),
+        FixedPointInitializer(0), ],
+)
+@pytest.mark.parametrize(
+    "selector",
+    [
+        MaxInfoSelector(),
+        RandomSelector(),
+        UrrySelector(), ],
+)
+@pytest.mark.parametrize(
+    "estimator", [NumericalSearchEstimator(method=m) for m in NumericalSearchEstimator.methods])
+@pytest.mark.parametrize(
+    "stopper",
+    [
+        MaxItemStopper(30),
+        MinErrorStopper(0.4), ],
+)
+def test_infinite_selectors(
+    examinees: int,
+    test_size: int,
+    bank_size: int,
+    logistic_model: str,
+    initializer: Initializer,
+    selector: Selector,
+    estimator: Estimator,
+    stopper: Stopper,
+):
+    items = generate_item_bank(bank_size, itemtype=logistic_model)
+    responses = cat.random_response_vector(random.randint(1, test_size - 1))
+    administered_items = np.random.choice(bank_size, len(responses), replace=False)
+    est_theta = initializer.initialize()
+    selector.select(
+        items=items,
+        administered_items=administered_items,
+        est_theta=est_theta,
+    )
+    estimator.estimate(
+        items=items,
+        administered_items=administered_items,
+        response_vector=responses,
+        est_theta=est_theta,
+    )
+    stopper.stop(
+        administered_items=items[administered_items],
+        theta=est_theta,
+    )
 
-                        for stopper in [
-                            MinErrorStopper(0.4),
-                            MaxItemStopper(test_size),
-                        ]:
-                            for selector in infinite_selectors:
-                                items = generate_item_bank(bank_size, itemtype=logistic_model)
-                                yield one_simulation, items, examinees, initializer, selector, estimator, stopper
+    one_simulation(items, examinees, initializer, selector, estimator, stopper)
 
 
 def test_item_bank_generation():
     for items in [
-        generate_item_bank(5, "1PL"),
-        generate_item_bank(5, "2PL"),
-        generate_item_bank(5, "3PL"),
-        generate_item_bank(5, "3PL", corr=0),
-        generate_item_bank(5, "4PL"),
-    ]:
+            generate_item_bank(5, "1PL"),
+            generate_item_bank(5, "2PL"),
+            generate_item_bank(5, "3PL"),
+            generate_item_bank(5, "3PL", corr=0),
+            generate_item_bank(5, "4PL"), ]:
         irt.validate_item_bank(items, raise_err=True)
-
-    items = numpy.zeros(100)
+    items = np.zeros(100)
     irt.validate_item_bank(items)
     items = irt.normalize_item_bank(items)
     irt.validate_item_bank(items, raise_err=True)
@@ -132,12 +188,9 @@ def test_plots():
     s.simulate(initializer, selector, estimator, stopper, verbose=True)
 
     for item in s.items[0:10]:
-        yield plot.item_curve, item[0], item[1], item[2], item[
-            3], "Test plot", "icc", False, None, False
-        yield plot.item_curve, item[0], item[1], item[2], item[
-            3], "Test plot", "iic", True, None, False
-        yield plot.item_curve, item[0], item[1], item[2], item[
-            3], "Test plot", "both", True, None, False
+        plot.item_curve(item[0], item[1], item[2], item[3], "Test plot", "icc", False, None, False)
+        plot.item_curve(item[0], item[1], item[2], item[3], "Test plot", "iic", True, None, False)
+        plot.item_curve(item[0], item[1], item[2], item[3], "Test plot", "both", True, None, False)
         close("all")
 
     plot.gen3d_dataset_scatter(items=s.items, show=False)
@@ -160,10 +213,3 @@ def test_plots():
 
     # close all plots after testing
     close("all")
-
-
-# if __name__ == "__main__":
-#     unittest.main()
-    # test_item_bank_generation()
-    # test_simulations()
-    # test_plots()
