5a3640887d31f2dc852c8829587298fd78fc9801
Douglas De Rizzo Meneghetti <douglas.meneghetti@therealreal.com>
2025-12-07 22:52:52 -0300

    generate docs in gh action, no more version control

--- docs/_downloads/fc302e54d1b5c2c307879bce9e7083d7/introduction-1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-from catsim import ItemBank
-from catsim.plot import PlotType, item_curve
-
-item_bank = ItemBank.generate_item_bank(1)
-item = item_bank[0]
-item_curve(a=item[0], b=item[1], c=item[2], d=item[3], ptype=PlotType.BOTH)
-plt.show()
