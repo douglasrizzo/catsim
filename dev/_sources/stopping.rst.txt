9dab4eccf6c3b64313fd9710408b106e44fb0096
douglasrizzo <douglasrizzo@users.noreply.github.com>
2025-12-11 00:29:03 +0000

    Deploy dev documentation from ba4b5caca5ea8a99a2c22a1360bd22289b0475da ba4b5caca5ea8a99a2c22a1360bd22289b0475da

--- dev/_sources/stopping.rst.txt
+++ dev/_sources/stopping.rst.txt
@@ -2,11 +2,13 @@ Stopping Criteria -- :mod:`catsim.stopping`
 *******************************************
 
 All implemented classes in this module inherit from a base abstract class
-:py:class:`Stopper`. :py:class:`Simulator` allows that a custom stopping criterion be
+:py:class:`BaseStopper`. :py:class:`Simulator` allows that a custom stopping criterion be
 used during the simulation, as long as it also inherits from
-:py:class:`Stopper`.
+:py:class:`BaseStopper`.
 
-.. inheritance-diagram:: catsim.stopping
+.. inheritance-diagram:: catsim.stopping.BaseStopper catsim.stopping.TestLengthStopper catsim.stopping.MinErrorStopper catsim.stopping.ConfidenceIntervalStopper
+   :parts: 1
+   :top-classes: catsim._base.Simulable
 
 .. automodule:: catsim.stopping
    :members:
--- dev/plot_directive/plot-4.py
+++ dev/plot_directive/plot-4.py
@@ -4,12 +4,12 @@ from catsim import plot
 from catsim.initialization import RandomInitializer
 from catsim.selection import MaxInfoSelector
 from catsim.estimation import NumericalSearchEstimator
-from catsim.stopping import MaxItemStopper
+from catsim.stopping import MinErrorStopper
 from catsim.simulation import Simulator
 
 fig, axes = plt.subplots(2, 1, figsize=(7, 12))
 s = Simulator(ItemBank.generate_item_bank(100), 10)
-s.simulate(RandomInitializer(), MaxInfoSelector(), NumericalSearchEstimator(), MaxItemStopper(20))
+s.simulate(RandomInitializer(), MaxInfoSelector(), NumericalSearchEstimator(), MinErrorStopper(0.4, max_items=20))
 plot.test_progress(simulator=s, index=0, ax=axes[0])
 plot.test_progress(simulator=s, index=0, info=True, var=True, see=True, ax=axes[1])
 plt.tight_layout()
