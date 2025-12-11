010f0484116fae4ba4411bdec3541ea86526edf0
Douglas De Rizzo Meneghetti <douglas.meneghetti@therealreal.com>
2025-12-10 22:12:12 -0300

    remove initializer compatibility thing

--- src/catsim/initialization/__init__.py
+++ src/catsim/initialization/__init__.py
@@ -3,13 +3,9 @@
 from .base import BaseInitializer
 from .initialization import FixedPointInitializer, InitializationDistribution, RandomInitializer
 
-# Backward compatibility alias
-Initializer = BaseInitializer
-
 __all__ = [
   "BaseInitializer",
   "FixedPointInitializer",
   "InitializationDistribution",
-  "Initializer",  # Backward compatibility alias
   "RandomInitializer",
 ]
